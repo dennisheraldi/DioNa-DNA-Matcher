a712b5ae547f605b2e00edd75d0ae8de615a9588
wmk567 <16520402@std.stei.itb.ac.id>
2022-04-28 14:03:28 +0700

    perbaikan lcs

--- src/api/method/lcs.go
+++ src/api/method/lcs.go
@@ -1,6 +1,6 @@
 package method
 
-func LcsCount(p string, q string, x int, y int) int {
+func LcsCount(p string, q string, x int, y int) float32 {
 	if x == 0 || y == 0 {
 		return 0
 	} else if p[x-1] == q[y-1] {
@@ -17,5 +17,6 @@ func LcsCount(p string, q string, x int, y int) int {
 }
 
 func LcsResult(p string, q string) float32 {
-	return (float32(LcsCount(p, q, len(p), len(q)) / len(q)))
+	var result float32 = LcsCount(p, q, len(p), len(q)) / float32(len(p))
+	return result
 }
